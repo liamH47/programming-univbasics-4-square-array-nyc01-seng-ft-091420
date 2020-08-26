0f1a8c94227ccb0233cbcb2956ee336ac4b2b15a
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-26 19:35:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,15 @@
-def square_array(array)
-  array = [1,2,3]
-  
-end
 
+def square_array(array)
+  # your code here	  # your code here
+end 	  newArray=[]
+array.each do |numbers| # The element, contained within the pipes, 
+                        # is like a placeholder. Whatever you put in
+                        # the pipes will be used in the block to represent 
+                        #each element of the array in turn.
+
+
+numbers=numbers**2 # square
+newArray<<numbers # adding to the newArray
+end     #end for do
+newArray # call 
+end     #end for def
